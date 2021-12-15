49aa1f18f6642e9b808566ef3c8552b73d88798c
OneFJef <jef@project87.net>
2021-12-15 10:50:53 -0500

    imported and placed home in App() return

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
