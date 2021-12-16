1f0e3ec7fddba80daf18962e13a6cf48ca8b9095
OneFJef <jef@project87.net>
2021-12-16 13:32:31 -0500

    added routes for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+// import React from "react";
 import "./App.css";
-import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="work" element={<Work />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
