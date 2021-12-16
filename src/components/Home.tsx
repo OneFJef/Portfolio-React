d8f6432d574db826608c1f30f45309d30a91a9b7
OneFJef <jef@project87.net>
2021-12-16 13:34:20 -0500

    modified home layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,8 @@
-import * as React from "react";
+// import * as React from "react";
 import {
   Avatar,
   Box,
+  Link,
   List,
   ListItem,
   ListItemAvatar,
@@ -11,13 +12,57 @@ import {
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-const Portrait = require("../assets/portrait-gray.jpg");
+const Portrait = require("../assets/portrait1.jpg");
 
-const Quote = "Full-Stack Developer with a passion for solving problems."
+const Quote = "Full-Stack Developer with a passion for solving problems.";
+
+const Github = "https://github.com/OneFJef";
+const LinkedIn = "https://www.linkedin.com/in/jefmitchell/";
 
 export default function Home() {
   return (
     <div>
+      {/* Mobile View */}
+      <Box
+        sx={{
+          display: { xs: "flex", md: "flex", lg: "none" },
+          height: "100vh",
+          position: "relative",
+          marginTop: "-64px",
+          mx: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        <List>
+          <ListItemAvatar sx={{ display: "flex", justifyContent: "center" }}>
+            <Avatar
+              alt="Jef Mitchell"
+              src={Portrait}
+              sx={{ m: 1, width: 200, height: 200 }}
+            />
+          </ListItemAvatar>
+
+          <ListItem sx={{ justifyContent: "center" }}>
+            <Typography variant="h2">Jef Mitchell</Typography>
+          </ListItem>
+
+          <ListItem>
+            <Typography sx={{ color: "#8b8b8b", fontStyle: "italic" }}>
+              {Quote}
+            </Typography>
+          </ListItem>
+
+          <ListItem sx={{ justifyContent: "center" }}>
+            <ListItemIcon sx={{ color: "white" }}>
+              <GitHubIcon sx={{ fontSize: 30, pr: 1.5 }} />
+              <LinkedInIcon sx={{ fontSize: 30, pr: 1.5 }} />
+            </ListItemIcon>
+          </ListItem>
+        </List>
+      </Box>
+
       {/* Desktop View */}
       <Box
         sx={{
@@ -45,44 +90,6 @@ export default function Home() {
             </Typography>
           </ListItem>
           <ListItem>
-            <ListItemIcon sx={{ color: "white" }}>
-              <GitHubIcon sx={{ fontSize: 30, pr: 1.5 }} />
-              <LinkedInIcon sx={{ fontSize: 30, pr: 1.5 }} />
-            </ListItemIcon>
-          </ListItem>
-        </List>
-      </Box>
-
-      {/* Mobile View */}
-      <Box
-        sx={{
-          display: { xs: "flex", md: "flex", lg: "none" },
-          height: "100vh",
-          position: "relative",
-          marginTop: "-64px",
-          mx: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white",
-        }}
-      >
-        <List>
-          <ListItemAvatar sx={{ display: "flex", justifyContent: "center" }}>
-            <Avatar
-              alt="Jef Mitchell"
-              src={Portrait}
-              sx={{m:1, width: 200, height: 200 }}
-            />
-          </ListItemAvatar>
-          <ListItem sx={{ justifyContent: "center" }}>
-            <Typography variant="h2">Jef Mitchell</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography sx={{ color: "#8b8b8b", fontStyle: "italic" }}>
-              {Quote}
-            </Typography>
-          </ListItem>
-          <ListItem sx={{ justifyContent: "center" }}>
             <ListItemIcon sx={{ color: "white" }}>
               <GitHubIcon sx={{ fontSize: 30, pr: 1.5 }} />
               <LinkedInIcon sx={{ fontSize: 30, pr: 1.5 }} />
