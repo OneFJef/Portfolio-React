89d30f512337b871b085cfcbaca3ec93b3d1a174
OneFJef <jef@project87.net>
2021-12-18 07:52:04 -0500

    removed browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -9,15 +9,15 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="work" element={<Work />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
-    </BrowserRouter>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="work" element={<Work />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
 
