8384dd29b25b1c118b514fa93cdaa153d703004f
OneFJef <jef@project87.net>
2021-12-18 06:09:11 -0500

    added desktop view and header

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { Box, Button, List, ListItem, styled, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  List,
+  ListItem,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import emailjs from "emailjs-com";
 
@@ -99,12 +107,82 @@ export default function Contact() {
         sx={{
           display: { xs: "flex", md: "flex", lg: "none" },
           height: "100vh",
-          mt: 10,
           justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        <List sx={{ mt:4, width: "90%", maxWidth: 500 }}>
+          <ListItem>
+            <Typography variant="h4" sx={{  fontWeight: "bold" }}>
+              Get in Touch
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <WhiteTextField
+              fullWidth
+              label="Name"
+              variant="outlined"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              error={nameValidation}
+              helperText={nameValidation && "Please enter a name."}
+            />
+          </ListItem>
+          <ListItem>
+            <WhiteTextField
+              fullWidth
+              label="Email"
+              variant="outlined"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              error={emailValidation}
+              helperText={emailValidation && "Please enter an email."}
+            />
+          </ListItem>
+          <ListItem>
+            <WhiteTextField
+              fullWidth
+              multiline
+              rows={12}
+              label="Message"
+              variant="outlined"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              error={messageValidation}
+              helperText={messageValidation && "Please enter a message."}
+            />
+          </ListItem>
+          <ListItem sx={{ justifyContent: "end" }}>
+            <WhiteButton
+              type="submit"
+              variant="outlined"
+              startIcon={<SendIcon />}
+              onClick={validate}
+            >
+              Send
+            </WhiteButton>
+          </ListItem>
+        </List>
+      </Box>
+
+      {/* Desktop View */}
+      <Box
+        sx={{
+          display: { xs: "none", md: "none", lg: "flex" },
+          height: "100vh",
+          marginLeft: "400px",
+          justifyContent: "center",
+          alignItems: "center",
           color: "white",
         }}
       >
         <List sx={{ width: "90%", maxWidth: 500 }}>
+          <ListItem>
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              Get in Touch
+            </Typography>
+          </ListItem>
           <ListItem>
             <WhiteTextField
               fullWidth
