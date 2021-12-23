72c1227fea77d54c88d6513d132c01b4552c4977
OneFJef <jef@project87.net>
2021-12-23 02:43:27 -0500

    added tech badges

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,6 +20,65 @@ When I'm not working with technology, I'm adventurous and passionate about being
 who have endearingly nicknamed me the "Crazy Cat Lady" I adore my three cats, Oscar, Panda, and Ygritte, 
 and have even had the opportunity to foster animals in need.`;
 
+const badges = [
+  {
+    tech: "mongodb",
+    link: "https://img.shields.io/badge/-MongoDB-47A248?logo=mongodb&logoColor=white",
+  },
+  {
+    tech: "express",
+    link: "https://img.shields.io/badge/-Express-000000?logo=express&logoColor=white",
+  },
+  {
+    tech: "reactjs",
+    link: "https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white",
+  },
+  {
+    tech: "nodejs",
+    link: "https://img.shields.io/badge/-Node.js-339933?logo=node.js&logoColor=white",
+  },
+  {
+    tech: "html",
+    link: "https://img.shields.io/badge/-HTML5-E34F26?logo=html5&logoColor=white",
+  },
+  {
+    tech: "css",
+    link: "https://img.shields.io/badge/-CSS3-1572B6?logo=css3&logoColor=white",
+  },
+  {
+    tech: "javascript",
+    link: "https://img.shields.io/badge/-Javascript-F7DF1E?logo=javascript&logoColor=white",
+  },
+  {
+    tech: "typescript",
+    link: "https://img.shields.io/badge/-Typescript-3178C6?logo=typescript&logoColor=white",
+  },
+  {
+    tech: "mysql",
+    link: "https://img.shields.io/badge/-MySQL-4479A1?logo=mysql&logoColor=white",
+  },
+  {
+    tech: "git",
+    link: "https://img.shields.io/badge/-Git-F05032?logo=git&logoColor=white",
+  },
+  {
+    tech: "bootstrap",
+    link: "https://img.shields.io/badge/-Bootstrap-7952B3?logo=bootstrap&logoColor=white",
+  },
+  {
+    tech: "bulma",
+    link: "https://img.shields.io/badge/-Bulma-00D1B2?logo=bulma&logoColor=white",
+  },
+  {
+    tech: "mui",
+    link: "https://img.shields.io/badge/-MUI-007FFF?logo=mui&logoColor=white",
+  },
+  {
+    tech: "npm",
+    link: "https://img.shields.io/badge/-npm-CB3837?logo=npm&logoColor=white",
+  },
+];
+
 export default function About() {
   return (
     <div>
@@ -69,6 +128,21 @@ export default function About() {
                 >
                   {Story}
                 </Typography>
+              </ListItem>
+              <ListItem>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    minWidth: 300,
+                    maxWidth: 650,
+                  }}
+                >
+                  {badges.map((tech) => (
+                    <img className="techBadge" src={tech.link} />
+                  ))}
+                </Box>
               </ListItem>
             </List>
           </Box>
@@ -120,6 +194,21 @@ export default function About() {
                 >
                   {Story}
                 </Typography>
+              </ListItem>
+              <ListItem>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    minWidth: 300,
+                    maxWidth: 650,
+                  }}
+                >
+                  {badges.map((tech) => (
+                    <img className="techBadge" src={tech.link} />
+                  ))}
+                </Box>
               </ListItem>
             </List>
           </Box>
